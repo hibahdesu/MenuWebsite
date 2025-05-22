37990da0ac7a65024211119bb80862cbbde7c180
hibahdesu <hibahsindi@gmail.com>
2025-05-22 20:16:27 +0300

    Update: posts and articles

--- src/PostsAndSidemnu.jsx
+++ src/PostsAndSidemnu.jsx
@@ -5,17 +5,172 @@ import Title from './Title';
 
 
 export default function PostsAndSidemnu() {
+    const posts = [
+        {
+            id: 1,
+            title: "Classic Pancake",
+            price: "$4.50",
+            image: "./images/Classic Pancake.png"
+        },
+        {
+            id: 2,
+            title: "Pancake & Coffee",
+            price: "$6.50",
+            image: "./images/Pancake & Coffee.png"
+        },
+        {
+            id: 3,
+            title: "Bear Pancake",
+            price: "$5.50",
+            image: "./images/Bear Pancake.png"
+        },
+        {
+            id: 4,
+            title: "Fresh sandwich",
+            price: "$6.00",
+            image: "./images/Fresh sandwich.png"
+        },
+        {
+            id: 5,
+            title: "Hot Dog Sandwich",
+            price: "$4.00",
+            image: "./images/Hot Dog Sandwich.png"
+        },
+        {
+            id: 6,
+            title: "Classic Omelet",
+            price: "$5.50",
+            image: "./images/Classic Omelet.png"
+        },
+        {
+            id: 7,
+            title: "Beef Burger",
+            price: "$7.50",
+            image: "./images/Beef Burger.png"
+        },
+        {
+            id: 8,
+            title: "Veggie Burger",
+            price: "$7.00",
+            image: "./images/Veggie Burger.png"
+        },
+        {
+            id: 9,
+            title: "Chicken Burger",
+            price: "$7.00",
+            image: "./images/Chicken Burger.png"
+        },
+        {
+            id: 10,
+            title: "Combo Burger",
+            price: "$9.00",
+            image: "./images/Combo Burger.png"
+        },
+        {
+            id: 11,
+            title: "French Fries",
+            price: "$3.00",
+            image: "./images/French Fries.png"
+        },
+        {
+            id: 12,
+            title: "Beef Taco",
+            price: "$4.00",
+            image: "./images/Beef Taco.png"
+        },
+        {
+          id: 13,
+          title: "Cat Pizza",
+          price: "$8.00",
+          image: "./images/Cat Pizza.png"
+        },
+        {
+          id: 14,
+          title: "Bacon Pizza",
+          price: "$8.50",
+          image: "./images/Bacon Pizza.png"
+        },
+        {
+          id: 15,
+          title: "Vegetarian Pizza",
+          price: "$8.00",
+          image: "./images/Vegetarian Pizza.png"
+        },
+        {
+            id: 16,
+            title: "Meat Bao",
+            price: "$6.50",
+            image: "./images/Meat Bao.png"
+        },
+        {
+            id: 17,
+            title: "Sweet Mochi",
+            price: "$4.00",
+            image: "./images/Sweet Mochi.png"
+        },
+        {
+            id: 18,
+            title: "Strawberry Mocha",
+            price: "$4.00",
+            image: "./images/Strawberry Mocha.png"
+        },
+        {
+            id: 19,
+            title: "Classic Cupcake",
+            price: "$3.00",
+            image: "./images/Classic Cupcake.png"
+        },
+        {
+            id: 20,
+            title: "Mocha Cake",
+            price: "$4.50",
+            image: "./images/Mocha Cake.png"
+        },
+        {
+            id: 21,
+            title: "Cherry Cake",
+            price: "$4.50",
+            image: "./images/Cherry Cake.png"
+        },
+        {
+            id: 22,
+            title: "Bear Cake",
+            price: "$4.50",
+            image: "./images/Bear Cake.png"
+        },
+        {
+          id: 23,
+          title: "Cookie Cake",
+          price: "$4.50",
+          image: "./images/Cookie Cake.png"
+        },
+        {
+          id: 24,
+          title: "Biscuit Cake",
+          price: "$4.50",
+          image: "./images/Biscuit Cake.png"
+        }
+    ];
+
+    const postsList = posts.map((post) => (
+        <Post
+            key={post.id}
+            title={post.title}
+            price={post.price}
+            image={post.image}
+        />
+    ));
+        
     return (
         <div className='recipe-section'>
         {/* Posts and Side Menu */}
         <div className='posts-and-side-menu'>
-          
-
           {/* Posts*/}
           <div className='posts-container'>
             <Title title={"All items"} />
             <div className='posts'>
-              <Post title={"Classic Pancake"} price={"$4.50"} image="./images/Classic Pancake.png" />
+                {postsList}
+              {/* <Post title={"Classic Pancake"} price={"$4.50"} image="./images/Classic Pancake.png" />
               <Post title={"Pancake & Coffee"} price={"$6.50"} image="./images/Pancake & Coffee.png" />
               <Post title={"Bear Pancake"} price={"$5.50"} image="./images/Bear Pancake.png" /> 
               <Post title={"Fresh sandwich"} price={"$6.00"} image="./images/Fresh sandwich.png" />
@@ -40,7 +195,7 @@ export default function PostsAndSidemnu() {
               <Post title={"Cherry Cake"} price={"$4.50"} image="./images/Cherry Cake.png" />
               <Post title={"Bear Cake"} price={"$4.50"} image="./images/Bear Cake.png" />
               <Post title={"Cookie Cake"} price={"$4.50"} image="./images/Cookie Cake.png" />
-              <Post title={"Biscuit Cake"} price={"$4.50"} image="./images/Biscuit Cake.png" />
+              <Post title={"Biscuit Cake"} price={"$4.50"} image="./images/Biscuit Cake.png" /> */}
             </div>
           </div>
 
--- src/SideMenu.jsx
+++ src/SideMenu.jsx
@@ -3,15 +3,59 @@ import './SideMenu.css';
 import Articles from './Articles';
 
 export default function SideMenu() {
+    const articles = [
+        {
+            id: 1,
+            title: "Craving Pizza? These Slices Speak for Themselves",
+            image: "./images/article1.png"
+        },
+        {
+            id: 2,
+            title: "Pasta Lovers Rejoice: Saucy Goodness Awaits",
+            image: "./images/pasta.png"
+        },
+        {
+            id: 3,
+            title: "Burgers Built Better – From Bun to Bite",
+            image: "./images/burger.png"
+        },
+        {
+            id: 4,
+            title: "From Our Kitchen to Your Table: The Story Behind the Menu",
+            image: "./images/story.png"
+        },
+        {
+            id: 5,
+            title: "How We Craft Each Dish with Care",
+            image: "./images/dish.png"
+        },
+        {
+            id: 6,
+            title: "Sweet Endings: Desserts Worth Drooling Over",
+            image: "./images/dessert.png"
+        },
+        {
+            id: 7,
+            title: "Summer Bites & Cool Sips: Refreshing Menu Updates",
+            image: "./images/menu.png"
+        }
+    ];
+
+    const articleList = articles.map((article) => {
+        return (
+            <Articles key={article.id} image={article.image} title={article.title} />
+        )
+    });
     return (
         <div className='side-menu'>
-            <Articles image={"./images/article1.png"} title={"Craving Pizza? These Slices Speak for Themselves"} />
+            {/* <Articles image={"./images/article1.png"} title={"Craving Pizza? These Slices Speak for Themselves"} />
             <Articles image={"./images/article1.png"} title={"Pasta Lovers Rejoice: Saucy Goodness Awaits"} />
             <Articles image={"./images/article1.png"} title={"Burgers Built Better – From Bun to Bite"} />
             <Articles image={"./images/article1.png"} title={"From Our Kitchen to Your Table: The Story Behind the Menu"} />
             <Articles image={"./images/article1.png"} title={"How We Craft Each Dish with Care"} />
             <Articles image={"./images/article1.png"} title={"Sweet Endings: Desserts Worth Drooling Over"} />
-            <Articles image={"./images/article1.png"} title={"Summer Bites & Cool Sips: Refreshing Menu Updates"} />
+            <Articles image={"./images/article1.png"} title={"Summer Bites & Cool Sips: Refreshing Menu Updates"} /> */}
+            {articleList}
         </div>
     )
 }
