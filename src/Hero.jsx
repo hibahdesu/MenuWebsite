40d50933f4e690f9ca8c9e3b2bb9b402093f98c6
hibahdesu <hibahsindi@gmail.com>
2025-06-09 23:47:58 +0300

    Update the style of hero section

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -4,7 +4,7 @@ import './Hero.css';
 export default function Hero() {
     return (
         <div className="hero">
-            <h1>Welcome to Our Website</h1>
+            <h1 className='hero-title'>Welcome to RestLife</h1>
             <img src="./images/hero.png" alt="Hero" className="hero-image" />
         </div>
     );
