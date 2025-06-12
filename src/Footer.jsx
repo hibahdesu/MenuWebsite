d615c06596ba00d87a7488fd4ec9308fc965a374
hibahdesu <hibahsindi@gmail.com>
2025-06-12 20:29:30 +0300

    Add: the style

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,12 +4,16 @@ export default function Footer() {
     return (
         <footer className="footer">
             <div className="footer-content">
-                <p>&copy; 2025 Pancake RestLife. All rights reserved.</p>
                 <ul className="social-links">
                     <li><a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">Facebook</a></li>
                     <li><a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">Twitter</a></li>
                     <li><a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">Instagram</a></li>
                 </ul>
+                <div className='footer-notes'>
+                    <p>&copy; 2025 Pancake RestLife. All rights reserved.</p>
+                    <p className="footer-note">Made and Designed with ❤️ by Hibah Sindi</p>
+                </div>
+                
             </div>
         </footer>
     );
