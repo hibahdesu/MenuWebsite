49f57957cc55b04f5026bd082312a8c7237ee031
hibahdesu <hibahsindi@gmail.com>
2025-06-13 18:02:08 +0300

    Update the styles

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,9 +1,10 @@
+import './Main.css';
 import './Hero.css';
 
 
 export default function Hero() {
     return (
-        <div className="hero">
+        <div className="hero wrapper">
             <h1 className='hero-title'>Welcome to RestLife</h1>
             <img src="./images/hero.png" alt="Hero" className="hero-image" />
         </div>
