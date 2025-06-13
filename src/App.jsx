88ab8d68c558036b2ed80bf78f173250b78a7224
hibahdesu <hibahsindi@gmail.com>
2025-06-13 14:31:39 +0300

    Update the style

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
+import './Main.css';
 import './App.css';
 import Nav from './Nav';
-// import Header from './Header';
-// import Post from './Post';
-// import SideMenu from './SideMenu';
-// import Title from './Title';
 import PostsAndSidemnu from './PostsAndSidemnu';
 import FilterButtons from './FilterButtons';
 import Hero from './Hero';
@@ -13,7 +10,6 @@ function App() {
   return (
     <div className="App">
       <Nav />
-      {/* <Header /> */}
       <Hero /> 
       <FilterButtons 
         filters={['All', 'Breakfast', 'Lunch', 'Dinner']} 
