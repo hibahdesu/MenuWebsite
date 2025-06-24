8b1d63030b5e24c59be44c320c62fa6b7e019811
hibahdesu <hibahsindi@gmail.com>
2025-06-24 23:08:18 +0300

    Update the styles

--- src/ArticleDetails.jsx
+++ src/ArticleDetails.jsx
@@ -1,3 +1,5 @@
+import './ArticleDetails.css'
+import './Main.css'
 import { useParams } from "react-router-dom";
 import { useContext } from "react";
 import { articles } from "./contexts/articlesContext"
@@ -15,8 +17,7 @@ export default function ArticleDetails() {
     
 
     return (
-        <div>
-        <h1>Article Details Page</h1>
+        <div className="article-details-container wrapper">
         <Title title={ article.title }/>
         <img src={article.image} alt="" className="detail-img"/>
         <p> {article.content}</p>
