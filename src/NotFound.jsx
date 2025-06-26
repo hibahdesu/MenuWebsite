7709fb35dadd18e47b3c4af03bf51383273544a8
hibahdesu <hibahsindi@gmail.com>
2025-06-26 21:03:20 +0300

    Add the link to home page

--- src/NotFound.jsx
+++ src/NotFound.jsx
@@ -1,8 +1,13 @@
-import './NotFound.css'
+import './NotFound.css';
+import { Link } from 'react-router-dom';
 export default function NotFound() {
     return (
         <div>
             <h1>Page Not Found</h1>
+            <Link to={'/'}>
+             <button>Return to the Home page</button>
+             </Link>
+           
         </div>
     )
 }
