8c8671c64e23b0256eff32fff2399377cc2067ce
hibahdesu <hibahsindi@gmail.com>
2025-06-27 17:47:01 +0300

    Update the footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,14 +1,32 @@
 import './Main.css';
 import './Footer.css';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import XIcon from '@mui/icons-material/X';
+import FacebookIcon from '@mui/icons-material/Facebook';
+
 
 export default function Footer() {
     return (
         <footer className="footer wrapper">
             <div className="footer-content">
                 <ul className="social-links">
-                    <li><a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">Facebook</a></li>
-                    <li><a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">Twitter</a></li>
-                    <li><a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">Instagram</a></li>
+                    <li className='flex'>
+                        <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+                            <InstagramIcon />
+                        </a>
+                    </li>
+                    <li className='flex'>
+                        
+                        <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
+                            <XIcon />
+                        </a>
+                    </li>
+                    <li>
+                        
+                        <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+                            <FacebookIcon />
+                        </a>
+                    </li>
                 </ul>
                 <div className='footer-notes'>
                     <p>&copy; 2025 Pancake RestLife. All rights reserved.</p>
